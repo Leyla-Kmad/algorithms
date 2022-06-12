20536df34d7c45663d118f31a9599a73708e0b9f
Leyla K <l.karamad@gmail.com>
2022-06-12 23:41:34 +0200

    random number function added

---
+++ randomNum.js
@@ -0,0 +1,7 @@
+function randomRange(myMin, myMax) {
+  
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+  // random Number between 0 and 9:
+  // return Math.floor(Math.random() * 10)
+}
